506b48e50f864232f8dcad97cdbd763a19398859
Chaslau Ustsinovich <chustsinovich@gmail.com>
2025-11-11 17:46:20 +0100

    Add project with math examples

---
+++ Lesson1/lesson1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            int num1 = 5;
+            int num2 = 8;
+            int simpleResult;
+            double num3 = 10.0;
+            double complResult;
+            bool boolResult;
+
+            simpleResult = num1 + num2;
+            Console.WriteLine(simpleResult);
+
+            num1 += num2;
+            Console.WriteLine(num1);
+
+            simpleResult = num2 - num1;
+            Console.WriteLine(simpleResult);
+
+            num2 -= num1;
+            Console.WriteLine(num2);
+
+            simpleResult = num1 * num2;
+            Console.WriteLine(simpleResult);
+
+            complResult = (double)num1 / num2;
+            Console.WriteLine(complResult);
+
+            complResult = num3 % 4.0;
+            Console.WriteLine(complResult);
+
+            boolResult = num1 > num2;
+            Console.WriteLine(boolResult);
+
+            boolResult = num1 < num2;
+            Console.WriteLine(boolResult);
+
+            boolResult = num1 == num2;
+            Console.WriteLine(boolResult);
+
+
+        }
+    }
+}
