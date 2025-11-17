5fd9e3dc8164410c017d198970f33cf169efda8c
Chaslau Ustsinovich <chustsinovich@gmail.com>
2025-11-17 17:09:01 +0100

    Add , == .

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Calculator
+﻿using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Calculator
 {
     internal class Program
     {
@@ -13,32 +16,40 @@
 
                 switch (selection)
                 {
-                    case "1":
+                    case "1":                        
                         Console.Write("Enter the first number: ");
-                        double sumNum1 = Convert.ToDouble(Console.ReadLine());
+                        double sumNum1 = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
                         Console.Write("Enter the second number: ");
-                        double sumNum2 = Convert.ToDouble(Console.ReadLine());
+                        double sumNum2 = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
                         Console.WriteLine($"Result: {sumNum1 + sumNum2}");
                         break;
                     case "2":
                         Console.Write("Enter the first number: ");
-                        double substrNum1 = Convert.ToDouble(Console.ReadLine());
+                        double substrNum1 = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
                         Console.Write("Enter the second number: ");
-                        double substrNum2 = Convert.ToDouble(Console.ReadLine());
+                        double substrNum2 = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
                         Console.WriteLine($"Result: {substrNum1 - substrNum2}");
                         break;
                     case "3":
                         Console.Write("Enter the first number: ");
-                        double multNum1 = Convert.ToDouble(Console.ReadLine());
+                        double multNum1 = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
                         Console.Write("Enter the second number: ");
-                        double multNum2 = Convert.ToDouble(Console.ReadLine());
+                        double multNum2 = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
                         Console.WriteLine($"Result: {multNum1 * multNum2}");
                         break;
                     case "4":
                         Console.Write("Enter the first number: ");
-                        double divNum1 = Convert.ToDouble(Console.ReadLine());
+                        double divNum1 = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
                         Console.Write("Enter the second number: ");
-                        double divNum2 = Convert.ToDouble(Console.ReadLine());
+                        double divNum2 = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
                         if (divNum2 != 0)
                         {
                             Console.WriteLine($"Result: {divNum1 / divNum2}");
@@ -50,23 +61,26 @@
                         break;
                     case "5":
                         Console.Write("Enter the first number: ");
-                        double modNum1 = Convert.ToDouble(Console.ReadLine());
+                        double modNum1 = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
                         Console.Write("Enter the second number: ");
-                        double modNum2 = Convert.ToDouble(Console.ReadLine());
+                        double modNum2 = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
                         Console.WriteLine($"Result: {modNum1 % modNum2}");
                         break;
                     case "6":
                         Console.Write("Enter the number to find the square root of number: ");
-                        double rootNum = Convert.ToDouble(Console.ReadLine());
-                       
-                            if (rootNum >= 0)
-                            {
-                                Console.WriteLine($"Result: {Math.Sqrt(rootNum)}");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Error: Cannot calculate the square root of a negative number.");
-                            }
+                        double rootNum = Convert.ToDouble(Console.ReadLine().Replace(',', '.'),
+                        System.Globalization.CultureInfo.InvariantCulture);
+
+                        if (rootNum >= 0)
+                        {
+                            Console.WriteLine($"Result: {Math.Sqrt(rootNum)}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Cannot calculate the square root of a negative number.");
+                        }
                         break;
                     case "Q":
                     case "q":
@@ -75,7 +89,7 @@
                     default:
                         Console.WriteLine("Invalid selection. Please try again.");
                         continue;
-                }                
+                }
             }
         }
     }
